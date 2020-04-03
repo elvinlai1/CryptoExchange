6bca4c71faa44772dc47c0096b3ac3b279064974
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-03 01:27:00 -0700

    Add files via upload

---
+++ public/Homepage.php
@@ -0,0 +1,40 @@
+Homepage.php
+
+
+<?php
+
+include "../app/partials/header.php"; 
+
+?>
+
+<body>
+  <div id="app">
+    <ul id="nav">
+      <li><a class="active" href="#home">Home</a></li>
+      <li><a href="#news">News</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#about">About</a></li>
+      <li id="LoginRegister"><a href="#login">Log in</a></li>
+      <li id="LoginRegister"><a href="#signup">Sign up</a></li>
+    </ul>
+  </div>
+  <div class=bodyframe>
+    <h1>
+      The Best Website <p>for</p> Simple Investment
+    </h1>
+    <h2>
+      There is no better startline than here.
+    </h2>
+    <button class="button" style="vertical-align:middle">
+      <span> Get Started </span>
+    </button>
+  </div>
+  <div class=cookies>
+  </div>
+</body>
+
+<?php 
+
+include "../app/partials/footer.php"; 
+
+?> 
