4b53d95aa864af0702d1b6cd7ee15cdd1598e8e1
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-04 22:39:32 -0700

    Update Login.php

--- public/Login.php
+++ public/Login.php
@@ -41,9 +41,7 @@
                   <div class="form-group">
                     <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
                     <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
-                  </div>
-                  <div id="register-link" class="text-right">
-                    <a href="#" class="text-info">Register here</a>
+                    <input type="reset">
                   </div>
                 </form>
               </div>
