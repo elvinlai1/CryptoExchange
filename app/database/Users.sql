81ebcf5822522c3ce2096d2eb72e79fa8723cdec
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-05 16:58:59 -0700

    Update Users.sql

--- app/database/Users.sql
+++ app/database/Users.sql
@@ -3,6 +3,7 @@ USE CryptoTrader;
 
 CREATE TABLE Users(
     AccountID           VarChar(36)     NOT NULL,
+    AccountType         VarChar(36)     NOT NULL,
     LoginID             VarChar(36)     NOT NULL,
     Password            VarChar(255)    NOT NULL,
     FirstName           VarChar(25)     NOT NULL,
