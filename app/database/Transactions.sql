335a788d168ff8f1e86209dd6d687b79d23b357d
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-05 16:25:07 -0700

    Create Transactions.sql

---
+++ app/database/Transactions.sql
@@ -0,0 +1,12 @@
+USE CryptoTrader; 
+
+CREATE TABLE Transaction(
+    TransactionID       VarChar(36)     NOT NULL,
+    AccountID           VarChar(36)     NOT NULL,
+    CryptoID            VarChar(255)    NOT NULL,
+    PurchasePrice       VarChar(25)     NOT NULL,
+    PurchaseAmount      INT(10)         NOT NULL,
+    CONSTRAINT PK_Transaction PRIMARY KEY (TransactionID)
+    CONSTRAINT FK_Account FOREIGN KEY (AccountID) REFERENCES Users(AccountID)
+    CONSTRAINT FK_Crypto FOREIGN KEY (CryptoID) REFERENCES Cryptos(CryptoID)
+    );
