f44a4c247cec9ae881d2d7a2d9155f919457c411
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-05 16:12:09 -0700

    Update and rename cryptos.sql to Cryptos.sql

--- app/database/Cryptos.sql
+++ app/database/Cryptos.sql
@@ -1,11 +1,11 @@
 USE CryptoTrader; 
 
-CREATE TABLE cryptos(
+CREATE TABLE Cryptos(
     CryptoID            VarChar(36)     NOT NULL,
     Name                VarChar(36)     NOT NULL,
     Price               Decimal(10)     NOT NULL,
     Change              VarChar(10)     NOT NULL,
     MarketCapital       VarChar(10)     NOT NULL,
-    CONSTRAINT          CRYPTO_PK       PRIMARY KEY(CryptoID)                      
+    PRIMARY KEY(CryptoID)                      
     );
     
