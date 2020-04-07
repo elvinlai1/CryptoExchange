fc6e6d0e4a846de2087836b9dc7ccc0f13087bab
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-06 20:38:30 -0700

    Update Wallet.php

--- public/Wallet.php
+++ public/Wallet.php
@@ -1,46 +1,41 @@
-<?php
-
-
-
-class Wallet {
-    
-    public $coinname;
-    public $amount;
-
-
-    public function __construct($coinname, $amount) {
-        $this->coinname = $coinname; 
-        $this->amount = $amount;
-    }
-
-    public function getAmount(){
-
-        //SQL query
-        require '../app/database/database.php';
-        $connection = mysqli_connect("localhost", "root", "");
-        $db = mysqli_select_db($connection, 'dbName');
-        
-        $amount = $db_run("SELECT CoinTotal FROM Wallet");
-        $query_run = mysqli_query($connection, $amount) or die("Bad Query: $amount");
-        
-        return $this->$row['CoinTotal'];
-
-    }
-
-
-    public function getCoinName(){
-
-        //SQL query
-        require '../app/database/database.php';
-        $connection = mysqli_connect("localhost", "root", "");
-        $db = mysqli_select_db($connection, 'dbName');
-        
-        $coinname = $db_run("SELECT CoinName FROM Wallet");
-        $query_run = mysqli_query($connection, $coinname) or die("Bad Query: $transaction");
-        
-        return $this->$row['CoinName'];
-    }
-    
-}
-
+<?php 
+include '../app/partial/header.php'; 
+include '../app/partial/navbar.php'; 
 ?>
+
+<body>
+
+<h1>Your Wallet</h1>
+
+<div class="container">
+    <table>
+    <tr>
+        <th>Crypto</th>
+        <th>Amount</th>
+    </tr>
+
+<?php
+require '../app/database/database.php';
+
+$connection = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($connection, 'CryptoTrader');
+
+
+$wallet = $db_run("SELECT CoinName, CoinTotal FROM Wallet");
+$query_run = mysqli_query($connection, $wallet) or die("Bad Query: $transaction");
+
+// output data of each row
+while($row = mysqli_fetch_assoc($query_run)) {
+    ?>
+    <tr>
+        <td> <?php echo $row['CoinName'] ?></td>
+        <td> <?php echo $row['CoinTotal'] ?></td>
+    </tr>
+    <?php
+}
+?>
+</div>
+
+</body>
+
+<?php include '../app/partial/footer.php'; ?>
