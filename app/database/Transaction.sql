36784975026d106347aee3a0be9ba3d6ef531ffa
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-06 21:14:05 -0700

    Update Transaction.sql

--- app/database/Transaction.sql
+++ app/database/Transaction.sql
@@ -4,6 +4,7 @@ CREATE TABLE Transaction(
     TransactionID       VarChar(36)     NOT NULL,
     AccountID           VarChar(36)     NOT NULL,
     CryptoID            VarChar(255)    NOT NULL,
+    CryptoName          VarChar(36)     NOT NULL,
     PurchasePrice       VarChar(25)     NOT NULL,
     PurchaseAmount      INT(10)         NOT NULL,
     CONSTRAINT PK_Transaction PRIMARY KEY (TransactionID)
