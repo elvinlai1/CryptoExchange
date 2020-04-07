0e3b507613abaa0140f13f7cc9b87608270e2c86
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-06 19:30:50 -0700

    Update coinPrice.php

--- app/model/coinPrice.php
+++ app/model/coinPrice.php
@@ -1,15 +1,29 @@
 <?php
 
 
+
 class CoinPrice {
+    
+    public $coinname;
+    public $coinprice;
 
 
-    //Contructor 
-
+    public function __construct($coinname, $coinprice) {
+        $this->coinname = $coinname; 
+        $this->coinprice = $coinprice;
+    }
 
     public function getCoinPrice(){
 
         //SQL query
+        require '../app/database/database.php';
+        $connection = mysqli_connect("localhost", "root", "");
+        $db = mysqli_select_db($connection, 'dbName');
+        
+        $coinprice = $db_run("SELECT Price FROM Cryptos");
+        $query_run = mysqli_query($connection, $coinprice) or die("Bad Query: $transaction");
+        
+        return $this->$row['Price'];
 
     }
 
@@ -17,6 +31,14 @@ class CoinPrice {
     public function getCoinName(){
 
         //SQL query
+        require '../app/database/database.php';
+        $connection = mysqli_connect("localhost", "root", "");
+        $db = mysqli_select_db($connection, 'dbName');
+        
+        $coinname = $db_run("SELECT Name FROM Cryptos");
+        $query_run = mysqli_query($connection, $coinname) or die("Bad Query: $transaction");
+        
+        return $this->$row['Name'];
     }
     
 }
