3865299fa1e60d3e448dbe7651b0d58abd140008
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-06 22:19:01 -0700

    Update signUp.php

--- app/auth/signUp.php
+++ app/auth/signUp.php
@@ -1,33 +1,35 @@
+
 <?php 
 
-
+require "../app/database/database.php";
 //If signup button pressed
 //grab database file
 //check for empty fields
 //look up if database contains username and email already
 //password_hash()
 
-<?php
+
     session_start();
 
     // connect to database
-    $db = mysqli_connect("localhost", "root","", "authentication");
+    $connection = mysqli_connect("localhost", "root","", "authentication");
+    $db = mysqli_select_db($connection, 'Cry');
 
     if (isset($_POST[''register_btn])) {
         session_start();
         $username = mysql_real_escape_string($_POST['username']);
         $email = mysql_real_escape_string($_POST['email']);
         $password = mysql_real_escape_string($_POST['password']);
-        $password2 = mysql_real_escape_string($_POST['password2']);
+        $confirm = mysql_real_escape_string($_POST['confirm']);
 
-        if ($password == $password2) {
+        if ($password == $confirm) {
             // create username
             password = md5($password); //hash password before storing for security purpose
-            $sql = "INSERT INTO Users(username, email, password) VALUES ('$username', '$email', 'password')";
+            $sql = "INSERT INTO Users(username, email, password) VALUES ('$username', '$email', '$password')";
             mysqli_query($db, $sql);
             $_SESSION['message'] = "You are now logged in";
             $_SESSION['username'] = $username;
-            header("location: home.php"); //redirect to home page
+            header("location: ../public/Homepage.php"); //redirect to home page
         }
         else {
             $_SESSION['message'] = "Two passwords do not match";
