2bb17a36a4bae7acc5afbc4cce58c8f536f007df
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-06 21:29:06 -0700

    Update navbar.php

--- app/partials/navbar.php
+++ app/partials/navbar.php
@@ -7,14 +7,20 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="../public/Wallet.php">Wallet <span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <a class="nav-link" href="../public/ViewCrypto.php">Crypto <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" style="text-align: right" href="../public/Login.php">Log in</a>
+        <a class="nav-link" href="../public/ViewWallet.php">Wallet <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" style="text-align: right" href="../public/SignUp.php">Sign up</a>
+        <a class="nav-link" href="../public/ViewTransaction.php">Transaction <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="text-align: right" href="../public/Login.php">Log in <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="text-align: right" href="../public/SignUp.php">Sign up <span class="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
