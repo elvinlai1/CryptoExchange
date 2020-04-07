213aa80ea98d1b7953bdc7e56f37903e8890398d
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-07 13:32:46 -0700

    Update Cryptos.sql

--- app/database/Cryptos.sql
+++ app/database/Cryptos.sql
@@ -11,7 +11,7 @@ CREATE TABLE Cryptos(
     );
     
     
-    INSERT INTO Cryptos (CryptoID, Name, Price, PercentChange, Stock)
+INSERT INTO Cryptos (CryptoID, Name, Price, Change, Stock)
     VALUES  (1, 'Bitcoin', '10290.50', '0.0375', 100),
 	    (2, 'Ethereum', '242.40', '0.1489', 50),
             (3, 'XRP', '0.28', '0.0746', 75),
