f8bf37fc73209af111876cf96e6d4de100007ed5
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-06 20:25:11 -0700

    Update navbar.php

--- app/partials/navbar.php
+++ app/partials/navbar.php
@@ -1,6 +1,6 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="Homepage.php">Home</a>
+  <a class="navbar-brand" href="../public/Homepage.php">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -8,13 +8,13 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="Crypto.php">Crypto <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="../public/Crypto.php">Crypto <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" style="text-align: right" href="Login.php">Log in</a>
+        <a class="nav-link" style="text-align: right" href="../public/Login.php">Log in</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" style="text-align: right" href="SignUp.php">Sign up</a>
+        <a class="nav-link" style="text-align: right" href="../public/SignUp.php">Sign up</a>
       </li>
     </ul>
   </div>
