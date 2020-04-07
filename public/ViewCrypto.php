647da8d64df4369467025209cd2b2fa0991754ad
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-06 21:23:07 -0700

    Update ViewCrypto.php

--- public/ViewCrypto.php
+++ public/ViewCrypto.php
@@ -17,26 +17,30 @@ include '../app/partials/navbar.php';
         <th>MarketCap</th>
     </tr>
 
-
+<?php
 
 $crypto = $db_run("SELECT Name, Price, PercentChange, MarketCapital FROM Cryptos");
 $result = mysqli_query($db, $crypto) or die ("Bad Query: $crypto");
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        <?php
+        ?>
         <tr>
             <td> <?php echo $row['Name'] ?></td>
             <td> <?php echo $row['Price'] ?></td>
             <td> <?php echo $row['PercentChange'] ?></td>
             <td> <?php echo $row['MarketCapital'] ?></td>
         </tr>
-        ?>
+        <?php
     }
 } 
 else {
     echo "0 results";
 }
+
+?>
+
+
 
 <?php
 include '../app/partials/footer.php';
