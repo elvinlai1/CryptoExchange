85b4f6fa695469944001b45867cf3a22add810b7
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-07 14:47:47 -0700

    Update Cryptos.sql

--- app/database/Cryptos.sql
+++ app/database/Cryptos.sql
@@ -1,19 +1,24 @@
 USE CryptoTrader; 
 
-CREATE TABLE Cryptos(
-    CryptoID            VarChar(36)     NOT NULL,
-    Name                VarChar(36)     NOT NULL,
-    Price               Varchar(10)     NOT NULL,
-    Changes              VarChar(10)     NOT NULL,
-    Stock               Varchar(10)     NOT NULL,
-    MarketCapital       DECIMAL AS (Price * Stock),
-    CONSTRAINT PK_Cryptos PRIMARY KEY(CryptoID)                      
-    );
+CREATE TABLE Cryptos (
+	CryptoID INT NOT NULL,
+  	Name VARCHAR(45) NOT NULL,
+  	BeginningPrice DECIMAL(10,2) NOT NULL,
+  	Price DECIMAL (10,2),
+	PercentChange DECIMAL (10,6) AS (((Price - BeginningPrice) / BeginningPrice) * 100),
+  	Stock INT NOT NULL,
+  	MarketCapital DECIMAL AS (Price * Stock),
+  	PRIMARY KEY (CryptoID)
+);    
     
-    
-INSERT INTO Cryptos (CryptoID, Name, Price, Changes, Stock)
-    VALUES  ('1', 'Bitcoin', '10290.50', '0.0375', '100'),
-	    ('2', 'Ethereum', '242.40', '0.1489', '50'),
-            ('3', 'XRP', '0.28', '0.0746', '75'),
-            ('4', 'Tether', '1.40', '0.0017', '200');
-    
+INSERT INTO Cryptos (CryptoID, Name, BeginningPrice,Price, Stock)
+VALUES  (1, 'Bitcoin', 10290.50, 10290.50, 100),
+	(2, 'Ethereum', 242.40, 242.40, 50),
+        (3, 'XRP', 0.28,0.28, 75),
+        (4, 'Tether', 1.40, 1.40, 200);
+
+UPDATE Cryptos
+SET Price = 10288.80, Stock = 110
+WHERE Name = 'Bitcoin';
+
+SELECT * FROM Cryptos;
