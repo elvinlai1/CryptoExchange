ddc36089c6134362e3653d8a69591c7e90572656
Residevil <43865235+Residevil@users.noreply.github.com>
2020-04-06 21:10:56 -0700

    Update Wallet.sql

--- app/database/Wallet.sql
+++ app/database/Wallet.sql
@@ -3,8 +3,9 @@ USE CryptoTrader;
 CREATE TABLE Wallet(
     WalletID            VarChar(36)     NOT NULL,
     CryptoID            VarChar(36)     NOT NULL,
-    Cointotal           INT(5)          NOT NULL,
-    AccountID           VarChar(25)     NOT NULL,
+    CoinName            VarChar(36)     NOT NULL,
+    Cointotal           VarChar(36)     NOT NULL,
+    AccountID           VarChar(36)     NOT NULL,
     CONSTRAINT PK_Wallet PRIMARY KEY (WalletID),
     CONSTRAINT FK_Cryptos FOREIGN KEY (CryptoID)  REFERENCES cryptos(cryptoID),
     CONSTRAINT FK_Account FOREIGN KEY (AccountID) REFERENCES users(AccountID)
