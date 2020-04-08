ee981bb3f1e277980eba88acdc554ccda99bdaff
uhjey <61769259+uhjey@users.noreply.github.com>
2020-04-08 00:27:22 -0700

    Update Wallet.sql

--- app/database/Wallet.sql
+++ app/database/Wallet.sql
@@ -1,7 +1,7 @@
 USE CryptoTrader; 
 
 CREATE TABLE Wallet(
-    WalletID            VarChar(36)     NOT NULL,
+    WalletID            int NOT NULL    AUTO_INCREMENT,
     CryptoID            VarChar(36)     NOT NULL,
     CoinName            VarChar(36)     NOT NULL,
     Cointotal           VarChar(36)     NOT NULL,
@@ -9,17 +9,17 @@ CREATE TABLE Wallet(
     PRIMARY KEY (WalletID),
     FOREIGN KEY (AccID) REFERENCES Users(AccountID);
 
-INSERT INTO Wallet (WalletID, CryptoID, CoinName, Cointotal, AccID, )
-    VALUES  ('1', '1', 'Bitcoin', '1', '1'),
-            ('2', '2', 'Ethereum', '0', '2'),
-            ('3', '3', 'XRP', '2', '3'),
-            ('4', '4', 'Tether', '3', '4');
+INSERT INTO Wallet (CryptoID, CoinName, Cointotal, AccID, )
+    VALUES  ('1', 'Bitcoin', '1', '1'),
+            ('2', 'Ethereum', '0', '2'),
+            ('3', 'XRP', '2', '3'),
+            ('4', 'Tether', '3', '4');
 
 
 
 
-INSERT INTO Wallet (WalletID, CryptoID, CoinName, Cointotal, AccID)
-    VALUES  ('1', '1', 'Bitcoin', '1', '1'),
-            ('2', '2', 'Ethereum', '0', '2'),
-            ('3', '3', 'XRP', '2', '3'),
-            ('4', '4', 'Tether', '3', '4');
+INSERT INTO Wallet (CryptoID, CoinName, Cointotal, AccID)
+    VALUES  ('1', 'Bitcoin', '1', '1'),
+            ('2', 'Ethereum', '0', '2'),
+            ('3', 'XRP', '2', '3'),
+            ('4', 'Tether', '3', '4');
