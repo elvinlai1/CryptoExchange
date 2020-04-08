6f6658ea334cdb16d9db2301386869ec2eacfc87
uhjey <61769259+uhjey@users.noreply.github.com>
2020-04-07 19:51:14 -0700

    Update Funds.sql

--- app/database/Funds.sql
+++ app/database/Funds.sql
@@ -1,11 +1,11 @@
 USE CryptoTrader; 
 CREATE TABLE Funds (
-	Currency_Type VARCHAR(64) DEFAULT 'Canadian_Dollar',
+	Currency_Type VARCHAR(64) DEFAULT 'CAD',
 	Balance VARCHAR(64),
 	AccountID VARCHAR(64) NOT NUll
 );
-  	INSERT INTO Funds(Balance,AccountID)
-    VALUES  ('50000',1),
-            ('100000',2),
-            ('25000',3),
-            ('20',4),
+  	INSERT INTO Funds(Currency_Type,Balance,AccountID)
+    VALUES  (CAD,'50000',1),
+            (CAD,'100000',2),
+            (CAD,'25000',3),
+            (CAD,'20',4),
