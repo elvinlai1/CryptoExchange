6821e3ac1263dd3e763ef293ca32a4e75b0f76ea
laielvin0@gmail.com <laielvin0@gmail.com>
2020-04-07 23:56:54 -0700

    sql script changes

--- app/database/CreateCryptoTraderDatabase.sql
+++ app/database/CreateCryptoTraderDatabase.sql
@@ -8,8 +8,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS `crytotrader` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `crytotrader`;
+CREATE DATABASE IF NOT EXISTS `cryptotrader` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `cryptotrader`;
 
 CREATE TABLE `cryptos` (
   `CryptoID` varchar(36) NOT NULL,
