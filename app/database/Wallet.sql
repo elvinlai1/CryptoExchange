a51028bc2dc53d31c2100c47527a23e972e6b4dd
uhjey <61769259+uhjey@users.noreply.github.com>
2020-04-07 18:47:36 -0700

    Update Wallet.sql

--- app/database/Wallet.sql
+++ app/database/Wallet.sql
@@ -11,8 +11,8 @@ CREATE TABLE Wallet(
     PRIMARY KEY (WalletID),
     FOREIGN KEY (AccID) REFERENCES Users(AccountID);
 
-INSERT INTO Wallet (WalletID, CryptoID, CoinName, Cointotal, AccID, Balance)
-    VALUES  ('1', '1', 'Bitcoin', '1', '1', 10000),
-            ('2', '2', 'Ethereum', '0', '2', 340000),
-            ('3', '3', 'XRP', '2', '3', 0),
-            ('4', '4', 'Tether', '3', '4', 2000);
+INSERT INTO Wallet (WalletID, CryptoID, CoinName, Cointotal, AccID, Balance, Cash)
+    VALUES  ('1', '1', 'Bitcoin', '1', '1', 10000,'5000'),
+            ('2', '2', 'Ethereum', '0', '2', 340000,'5000'),
+            ('3', '3', 'XRP', '2', '3', 0, '5000'),
+            ('4', '4', 'Tether', '3', '4', 2000,'5000');
